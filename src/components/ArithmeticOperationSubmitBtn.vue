89f9e4e27479de4d87a78b46e4e668600e960d83
Jérémy Riverain <jeremy@geekco.fr>
2020-11-02 12:47:28 +0100

    fix bug computed submit icon

--- src/components/ArithmeticOperationSubmitBtn.vue
+++ src/components/ArithmeticOperationSubmitBtn.vue
@@ -4,30 +4,31 @@
     :disabled="!isPlaying"
     @click="$emit('submit')"
   >
-    <span class="icon is-small">
-      <i :class="icon"></i>
+    <span
+      class="icon is-small"
+      v-if="isPlaying"
+    >
+      <i class="fas fa-check"></i>
+    </span>
+    <span
+      class="icon is-small"
+      v-else
+    >
+      <i class="fas fa-question"></i>
     </span>
   </button>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "vue";
+import { defineComponent } from "vue";
 export default defineComponent({
   name: "ArithmeticOperationSubmitBtn",
   props: {
     isPlaying: {
       type: Boolean,
       required: true,
+      default: false,
     },
-  },
-  setup(props) {
-    const icon = computed(() =>
-      props.isPlaying ? "fas fa-check" : "fas fa-question"
-    );
-
-    return {
-      icon,
-    };
   },
 });
 </script>
